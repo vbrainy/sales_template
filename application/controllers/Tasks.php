08caed95c03f8db9bac240162b0fdb2a91ab7c99
vbrainy <vbrainy.solutions@gmail.com>
2016-05-01 16:06:09 +0530

    changes in button layout
    changes in button layout

--- application/controllers/Tasks.php
+++ application/controllers/Tasks.php
@@ -156,7 +156,7 @@ class Tasks extends CI_Controller {
 			$button .= '<a class="btn btn-danger deleteBtn" id="'.$r->id.'" data-toggle="tooltip" title="Delete">
 						<i class="fa fa-trash"></i> </a>';
                          $button .= '<a class="btn btn-info editBtn"  href="'.base_url('tasks/copytask/'. $r->id).'" data-toggle="tooltip" title="copy task">
-						<i class="fa "></i>Copy task</a>';
+						<i class="fa fa-copy"></i></a>';
 		        
                         $addjobbutton = '<a class="btn btn-primary editBtn" href="'.base_url('jobs/add_job/'. $r->id).'" data-toggle="tooltip" title="Add Job">
 						<i class="fa fa-plus"></i>&nbsp;Add Job </a>';
